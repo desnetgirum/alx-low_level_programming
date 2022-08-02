68a420fe0e8973032410cb4f9906c6bb44f66b6f
desnetgirum <desbe2020@gmail.com>
2022-08-02 16:04:21 -0700

    dog

--- x0E-structures_typedef/dog.h
+++ x0E-structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef STRTYP
+#define STRTYP
 
 /**
- *   * struct dog - Dog attributes
+ *   * struct dog - structure of a dog
  *     * @name: The name of the dog.
  *       * @age: The age of the dog.
  *         * @owner: The owner of the dog.
@@ -26,4 +26,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif/*STRTYP*/
