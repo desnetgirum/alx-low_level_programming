ec327cf9f9058baaba2ffb4608fb95e0494168eb
desnetgirum <desbe2020@gmail.com>
2022-09-29 16:27:47 -0700

    first

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,23 +1,25 @@
 /*
  *  * File: 1-dlistint_len.c
- *   * Auth: Desnet
+ *   * Auth: Desnet B Girum
  */
+
 #include "lists.h"
 
 /**
- *  * dlistint_len - returns the number of elements in a linked dlistint_t list.
- *   * @h: pointer to the list.
- *    * Return: number of nodes.
- **/
+ *  * dlistint_len - Counts the number of elements in a linked dlistint_t list.
+ *   * @h: The head of the dlistint_t list.
+ *    *
+ *     * Return: The number of elements in the dlistint_t list.
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t cont = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
-		cont++;
+		nodes++;
 		h = h->next;
 	}
 
-	return (cont);
+	return (nodes);
 }
