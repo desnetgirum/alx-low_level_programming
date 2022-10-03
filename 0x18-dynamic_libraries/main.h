b0e0fe45dfb81f1d12f58a8d5eb0aeb650f72aa2
desnetgirum <desbe2020@gmail.com>
2022-10-03 16:31:12 -0700

    uptodate

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _DYN_LIBS_
+#define _DYN_LIBS_
 
 int _putchar(char c);
 int _islower(int c);
@@ -24,4 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif /* main.h */
+
+#endif
